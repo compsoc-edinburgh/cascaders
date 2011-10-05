908d6cb149b90773666398cf766564353d90eb0d
Jacob Essex <jacob@jacobessex.com>
2011-10-05 18:55:01 +0100

    Added basic setup file

---
+++ client/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Cascaders',
+      version='1',
+      description='Helping people find cascaders in labs',
+      author='CompSoc Edinburgh',
+      author_email='compsoc-committee@googlegroups.com',
+      url='http://www.comp-soc.com',
+      packages=['cascaders'],
+     )
