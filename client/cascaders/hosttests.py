190b26c200f6d768f540e5fd8d24ca8bb7f15e31
Jacob Essex <jacob@jacobessex.com>
2011-10-06 14:50:31 +0100

    Fixed hosttest

--- client/cascaders/hosttests.py
+++ client/cascaders/hosttests.py
@@ -2,11 +2,10 @@
 
 '''
 This file is an attempt to check the hosts on the network are valid
-
-TODO this is broken atm. Doesn't cope if the program wants input
 '''
 
-import os
+import signal
+from subprocess import Popen, PIPE
 
 import labmap
 
@@ -17,14 +16,25 @@ with open(os.path.join(wd, 'data', 'hosts')) as f:
     for lab in locator.getLabs():
         for host, location in locator.getMap(lab):
 
-            (c_stdin, c_stdout, c_stderr) = os.popen3('ssh %s' % host,'r')
-            out = c_stdout.read()
-            err = c_stderr.read()
-            c_stdin.close()
-            c_stdout.close()
-            c_stderr.close()
+            class Alarm(Exception):
+                pass
 
-            if 'Name or service not known' in err:
+            def alarmHandler(signum, frame):
+                raise Alarm
+
+            signal.signal(signal.SIGALRM, alarmHandler)
+            signal.alarm(5)
+            terminated = False
+            try:
+                proc = Popen(["ssh", host], stdout=PIPE, stderr=PIPE)
+                stdout, stderr = proc.communicate()
+                signal.alarm(0)
+            except Alarm:
+                terminated = True
+                proc.terminate()
+
+            #We assume this is just asking for password if it is terminated
+            if terminated == False and  'Name or service not known' in stderr:
                 print 'Lookup of %s failed' % host
             else:
                 print 'Fine for %s' % host
